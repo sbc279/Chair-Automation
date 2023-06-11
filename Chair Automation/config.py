4dbf61d8a10ef2ad3e83c48c27d0759412b2ed3e
Stephen B Craver <steve@craver.com>
2023-06-11 16:17:06 -0400

    refactor: Post vacation - various issues
    refs: #522

--- Chair Automation/config.py
+++ Chair Automation/config.py
@@ -1,23 +1,24 @@
 from machine import Pin
 import machine
 # version...
-version = "2.0.5.30"
+version = "2.0.6.02"
 
+enablePrint = True
 enableLogging = False
 debugLog = "ChairLog.log"
 
-# Timing variables (seconds)...
-tm_failSafeSeconds = 60
-tm_out_to_home = 20
-tm_top_wait = 8
-tm_down_step = 8
+# Timing variables (float) seconds...
+tm_failSafeSeconds = float(60.0)
+tm_out_to_home = float(20.0)
+tm_top_wait = float(8.0)
+tm_down_step = float(8.0)
 tm_HomeOverRun = float(.30)
 tm_sw_bounce = float(.5)
-tm_1_wait = 1
-tm_10_wait = 8
-tm_5_wait = 5
+tm_1_wait = float(1.0)
+tm_10_wait = float(8.0)
+tm_5_wait = float(5.0)
 
-#   Status enums
+# enums
 id_none = 		0
 id_sw_Up = 		1
 id_sw_Up2 = 	2
@@ -31,43 +32,66 @@ id_led_home = 	256
 id_led_upper = 	512
 id_led_lower = 	1024
 id_led_occup = 	2048
+id_dn_runtime =	4096
+
+#   yeah, this is pretty much redundant
+ignore_none = 		0
+ignore_sw_Up = 		1
+ignore_sw_Up2 = 	2
+ignore_sw_Dn = 		4
+ignore_sw_Dn2 = 	8
+ignore_sw_Main_Up = 16
+ignore_sw_Main_Up2 = 32
+ignore_sw_Main_Dn = 64
+ignore_sw_Main_Dn2 = 128
+ignore_led_home = 	256
+ignore_led_upper = 	512
+ignore_led_lower = 	1024
+ignore_led_occup = 	2048
+
+#   enum control groups
+ignore_all_sw =     255
+ignore_allLimits =	3840
+ignore_all =		4095
 
 #	Control
-rly_Up 		= Pin(0, Pin.OUT)
-rly_Dn 		= Pin(1, Pin.OUT)
-sw_Main_Dn2 = Pin(6, Pin.IN,  pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Main_Up2 = Pin(7, Pin.IN,  pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Main_Dn	= Pin(9, Pin.IN,  pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Main_Up	= Pin(8, Pin.IN,  pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Up_2   	= Pin(12, Pin.IN, pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Dn_2   	= Pin(13, Pin.IN, pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Up   	= Pin(10, Pin.IN, pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
-sw_Dn   	= Pin(11, Pin.IN, pull = Pin.PULL_UP)   # enable internal pull-up resistor, set pin high on creation
+rly_Up 		= Pin(0,  Pin.OUT)                        # Main UP relay output
+rly_Dn 		= Pin(1,  Pin.OUT)                        # Main DOWN relay output
+sw_Home 	= Pin(2,  Pin.IN, pull = Pin.PULL_UP)     # Limit switch - enable internal pull-up resistor
+sw_Upper 	= Pin(3,  Pin.IN, pull = Pin.PULL_UP)     # Limit switch - enable internal pull-up resistor
+sw_Lower 	= Pin(4,  Pin.IN, pull = Pin.PULL_UP)     # Limit switch - enable internal pull-up resistor
+sw_Occup    = Pin(5,  Pin.IN, pull = Pin.PULL_UP)     # Limit switch - enable internal pull-up resistor
+sw_Main_Dn2 = Pin(6,  Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Main_Up2 = Pin(7,  Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Main_Dn	= Pin(8,  Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Main_Up	= Pin(9,  Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Up   	= Pin(10, Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Dn   	= Pin(11, Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Up_2   	= Pin(12, Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
+sw_Dn_2   	= Pin(13, Pin.IN, pull = Pin.PULL_DOWN)   # enable internal pull-down resistor
 
-#   Limit switches
-sw_Home 	= Pin(2, Pin.IN,  pull = Pin.PULL_UP)  # enable internal pull-up resistor, set pin high on creation
-sw_Upper 	= Pin(3, Pin.IN,  pull = Pin.PULL_UP)  # enable internal pull-up resistor, set pin high on creation
-sw_Lower 	= Pin(4, Pin.IN,  pull = Pin.PULL_UP)  # enable internal pull-up resistor, set pin high on creation
-sw_Occup    = Pin(5, Pin.IN,  pull = Pin.PULL_UP)  # enable internal pull-up resistor, set pin high on creation
+#	 NOTE: 'led_power' is tied directly to pin 36 (3.3vdc) and NOT under program control
+led_upper   = Pin(18, Pin.OUT) # 18=yel
+led_lower   = Pin(19, Pin.OUT) # 19=red
+led_home	= Pin(20, Pin.OUT) # 20=blu
+led_occup   = Pin(21, Pin.OUT) # 21=wht
 
-#	LED led_power = tied directly to pin 36 (3.3vdc)
-led_home	= Pin(20, Pin.OUT) # 18=yel 19=red 20=blu 21=wht
-led_upper   = Pin(18, Pin.OUT)
-led_lower   = Pin(19, Pin.OUT)
-led_occup   = Pin(21, Pin.OUT)
-
-#   All unused PICO GPIO pins are scoped INPUT and pulled HIGH/UP by default
-# unusedGpio = [14, 15, 16, 22, 26, 27]
-# for i in unusedGpio:
-#     Pin(i, Pin.IN, pull = Pin.PULL_UP)
+#   All unused PICO GPIO pins are scoped INPUT and pulled DOWN by default
+unusedGpio = [14, 15, 16, 17, 22]
+for i in unusedGpio:
+    Pin(i, Pin.IN, pull = Pin.PULL_DOWN)
 
 #   Misc global variables
 actual_time = '2023, 1, 1, 1, 5, 48, 1, 94' # just the format for now
 day = 0
-tm_Dn_Runtime = 0
+tm_Dn_Runtime = float(0.0)
 
 
 ON = False
 OFF = True
 UP = True
 DN = False
+
+
+
+
--- Chair Automation/functions.py
+++ Chair Automation/functions.py
@@ -1,4 +1,4 @@
-# version = "2.0.5.30"
+# version = 2.0.6.02
 
 import network
 import time
@@ -50,53 +50,53 @@ def printF(msg, msg2 = "", msg3 = "", msg4 = ""):
     formatted_time = "{:02}/{:02}/{:02} {:02}:{:02}:{:02}".format(tm[0], tm[1], tm[2], tm[3], tm[4], tm[5])
     mssg = "{}: {}{}{}{}"
     strng = mssg.format(formatted_time, msg, msg2, msg3, msg4)
-    print(strng)
+    if config.enablePrint:
+        print(strng)
     if config.enableLogging == True:
         f = open(config.debugLog, "a")
         f.write(strng + "\n")
         f.close()
          
          
-def Check_Button_Press():
-    trigger = 0
+def Check_Button_Press(checkRunTime = False, startTicks = float(0.0)):
+    #trigger = 0
     ret = 0
     
 # Is the switch pressed?    
     if config.sw_Up.value() == 1:
-        ret += config.id_sw_Up 			# 1
+        ret += config.ignore_sw_Up 			# 1
     if config.sw_Up_2.value() == 1:        
-        ret += config.id_sw_Up2 		# 2
+        ret += config.ignore_sw_Up2 		# 2
     if config.sw_Dn.value() == 1:
-        ret += config.id_sw_Dn 			# 4
+        ret += config.ignore_sw_Dn 			# 4
     if config.sw_Dn_2.value() == 1:        
-        ret += config.id_sw_Dn2 		# 8
-    if config.sw_Main_Up.value() == 0:
-        ret += config.id_sw_Main_Up 	# 16
-    if config.sw_Main_Up2.value() == 0:
-        ret += config.id_sw_Main_Up2 	# 32
-    if config.sw_Main_Dn.value() == 0:
-        ret += config.id_sw_Main_Dn 	# 64
-    if config.sw_Main_Dn2.value() == 0:
-        ret += config.id_sw_Main_Dn2 	# 128
+        ret += config.ignore_sw_Dn2 		# 8
+    if config.sw_Main_Up.value() == 1:
+        ret += config.ignore_sw_Main_Up 	# 16
+    if config.sw_Main_Up2.value() == 1:
+        ret += config.ignore_sw_Main_Up2 	# 32
+    if config.sw_Main_Dn.value() == 1:
+        ret += config.ignore_sw_Main_Dn 	# 64
+    if config.sw_Main_Dn2.value() == 1:
+        ret += config.ignore_sw_Main_Dn2 	# 128
     
 # Is the led on?
     if config.led_home.value() == 1:
-        ret += config.id_led_home  		# 256
+        ret += config.ignore_led_home  		# 256
     if config.led_upper.value() == 1:
-        ret += config.id_led_upper 		# 512
+        ret += config.ignore_led_upper 		# 512
     if config.led_lower.value() == 1:
-        ret += config.id_led_lower 		# 1024
+        ret += config.ignore_led_lower 		# 1024
     if config.led_occup.value() == 1:
-        ret += config.id_led_occup 		# 2048
-        
-#     if config.tm_Dn_Runtime > 0:
-#         ret += 4096
+        ret += config.ignore_led_occup 		# 2048
+ 
+    if checkRunTime == True:
+        rt = abs(config.tm_Dn_Runtime) - RunSeconds(startTicks, time.ticks_ms(), 2)
+        rt = round(rt, 2)
+        if rt > -0.1 and rt < 0.1:
+            ret += config.id_dn_runtime
     
     return ret
-
-# def resetIrqChange():
-#     for x in range(1, 2, 3, 4):
-#         config.chg(x) = False
 
 def RunSeconds(startTick, nowTick, precision = 2):
     return round((nowTick - startTick) / 1000, precision)
@@ -107,44 +107,81 @@ def IsPlural(number):
     else:
         return ""
     
-def Wait_Time(seconds, spc = 1, edgeDetect = 0, delay = 0): # 0=none, 1=up, 2=dn, 4=home, 8=both
-    # Compensate for PICO delay
-    picoDelay = .08
-    seconds = seconds - picoDelay
+def Wait_Time(seconds, spc = 1, ignoredBin = 0, ignoredDelay = 0, checkRunTime = False):
+    
+    # id_none = 		-1
+    # id_all =			0
+    # id_sw_Up = 		1
+    # id_sw_Up2 = 		2
+    # id_sw_Dn = 		4
+    # id_sw_Dn2 = 		8
+    # id_sw_Main_Up = 	16
+    # id_sw_Main_Up2 = 	32
+    # id_sw_Main_Dn = 	64
+    # id_sw_Main_Dn2 = 	128
+    # id_led_home = 	256
+    # id_led_upper = 	512
+    # id_led_lower = 	1024
+    # id_led_occup = 	2048    
+
+    # Compensate for PICO's latency
+    picoLatency = 0
+    
+    seconds = seconds - picoLatency
     spacer = Space(spc) + "wait -> "
     once = False
     tm = time.ticks_ms()
     trigger = 0
     exitReason = ""
-    edgeDetectStr = "FULL" 
+    ignoredBinStr = "\n\t\t" 
     homeValue = config.sw_Home.value()
-        
-    if edgeDetect == -1:     			# 0
-        edgeDetectStr ="All switches edge disabled for " + str(delay) + " second" + IsPlural(delay)    
-    if edgeDetect & config.id_sw_Up:    # 1
-        edgeDetectStr = "config.sw_Up edge disabled for " + str(delay) + " second" + IsPlural(delay)
-    if edgeDetect & config.id_sw_Dn:    # 2
-        edgeDetectStr = "config.sw_Dn edge disabled for " + str(delay) + " second" + IsPlural(delay)
-    if edgeDetect & (config.id_sw_Up2 or config.id_main_sw_Up2):
-        edgeDetectStr = "config.sw_main_Up edge disabled for " + str(delay) + " second" + IsPlural(delay)
-    if edgeDetect & (config.id_sw_Dn2 or config.id_main_sw_Dn2):
-        edgeDetectStr = "config.sw_main_Up edge disabled for " + str(delay) + " second" + IsPlural(delay)
 
-    if edgeDetect & config.id_led_home:  # 8
-        edgeDetectStr ="config.sw_Home edge disabled for " + str(delay) + " second" + IsPlural(delay)
-    if edgeDetect & config.id_led_upper:  # 8
-        edgeDetectStr ="config.sw_Upper edge disabled for " + str(delay) + " second" + IsPlural(delay)
-    if edgeDetect & config.id_led_lower:  # 8
-        edgeDetectStr ="config.sw_Lower edge disabled for " + str(delay) + " second" + IsPlural(delay)
-    if edgeDetect & config.id_led_occup:  # 8
-        edgeDetectStr ="config.sw_Occup edge disabled for " + str(delay) + " second" + IsPlural(delay)
+    if ignoredBin == -1: 	    			# 0
+        ignoredBinStr ="All switches edge enabled after " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) 
+    else:
+        if ignoredBin == 0: 	    			# 0
+            ignoredBinStr ="All switches edge disabled"
+        else:
+            if ignoredBin & config.ignore_sw_Up:    # 1
+                ignoredBinStr += spacer + "config.sw_Up edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_sw_Dn:    # 2
+                ignoredBinStr += spacer + "config.sw_Dn edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_sw_Up2:	# 4
+                ignoredBinStr += spacer + "config.sw_Up2 edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_sw_Dn2:	# 8
+                ignoredBinStr += spacer + "config.sw_Dn2 edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_sw_Main_Up: # 16
+                ignoredBinStr += spacer + "config.sw_Main_Up edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_sw_Main_Up2: # 32
+                ignoredBinStr += spacer + "config.sw_Main_Up2 edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"                
+            if ignoredBin & config.ignore_sw_Main_Dn: #64
+                ignoredBinStr += spacer + "config.sw_Main_Dn edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"    
+            if ignoredBin & config.ignore_sw_Main_Dn2: #128
+                ignoredBinStr += spacer + "config.sw_Main_Dn2 edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"    
+            if ignoredBin & config.ignore_led_home:  # 256
+                ignoredBinStr += spacer + "config.sw_Home edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_led_upper:  # 512
+                ignoredBinStr += spacer + "config.sw_Upper edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_led_lower:  # 1024
+                ignoredBinStr += spacer + "config.sw_Lower edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if ignoredBin & config.ignore_led_occup:  # 2048
+                ignoredBinStr += spacer + "config.sw_Occup edge disabled for " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay) + "\n\t\t"
+            if checkRunTime and ignoredBin & config.id_dn_runtime:  # 4096
+                ignoredBinStr += spacer + "tm_Dn_Runtime is being monitored" # after " + str(ignoredDelay) + " second" + IsPlural(ignoredDelay)
+
+
+
+            # strip off the trailing feed/tab, if needed        
+            if ignoredBinStr.endswith("\n\t\t"):
+               ignoredBinStr = ignoredBinStr.rstrip("\n\t\t") 
+    
         
-    printF(spacer, "Edge Detection: " + edgeDetectStr)
-    printF(spacer, "Duration: " + str(seconds + picoDelay) + " second" + IsPlural(delay) + " wait...")
+    printF(spacer, "Edge Detection: " + ignoredBinStr)
+    printF(spacer, "Duration: " + str(seconds + picoLatency) + " second" + IsPlural(ignoredDelay) + " wait...")
     
     complete = False
-    while trigger == 0:
-        if RunSeconds(tm, time.ticks_ms()) > config.tm_failSafeSeconds: # 60 second failsafe
+    while exitReason == "":
+        if round(config.tm_Dn_Runtime, 2) > config.tm_failSafeSeconds: # 60 second failsafe
             config.sw_Up.value(OFF)
             config.sw_Dn.value(OFF)
             config.rly_Up.value(OFF)
@@ -152,48 +189,63 @@ def Wait_Time(seconds, spc = 1, edgeDetect = 0, delay = 0): # 0=none, 1=up, 2=dn
             printF(spacer + " wait -> Complete FAILSAFE abort")
             return False
         
-        if not once:
-            printF(spacer, "interrupt delay enabled")
-            once = True
-    
-        if RunSeconds(tm, time.ticks_ms()) > delay: # Don't start checking until after "delay" (if it's not zero)
+        if checkRunTime == True:
+            rt = abs(config.tm_Dn_Runtime) - RunSeconds(tm, time.ticks_ms(), 2)
+            rt = round(rt, 2)
+            if rt > -0.5 and rt < 0.5:
+                trigger = config.id_dn_runtime
+                exitReason = "config.tm_Dn_Runtime reached 0"
+#         print(str(ignoredDelay))
+#         print(str(config.tm_Dn_Runtime))
+        ut = time.ticks_ms()
+        if RunSeconds(tm, ut, 2) >= ignoredDelay: # Don't start checking until after "ignoredDelay" (if it's not zero)
+            if not once:
+                if ignoredDelay > 0:
+                    printF(spacer, "Interrupt Delay enabled")
+                else:
+                    printF(spacer, "Interrupt Delay disabled")
+                once = True
             
-            trigger = Check_Button_Press()
-           
-        if trigger == 0 and RunSeconds(tm, time.ticks_ms(), 2) > seconds:
-            trigger = -1
-            exitReason = "Time (True)"
-     
-    if trigger == 1:
-        exitReason = "config.sw_Up interrupt"
-    if trigger == 2:
-        exitReason = "config.sw_Up2 iterrupt"            
-    if trigger == 4:
-        exitReason = "config.sw_Dn interrupt"
-    if trigger == 8:
-        exitReason = "config.sw_Dn2 iterrupt"            
-    if trigger == 16:
-        exitReason = "config.sw_Main_Up interrupt"
-    if trigger == 32:
-        exitReason = "config.sw_Main_Up2 iterrupt"
-    if trigger == 64:
-        exitReason = "config.sw_Main_Dn interrupt"
-    if trigger == 128:
-        exitReason = "config.sw_Main_Dn2 interrupt"
-    if trigger == 256:
-        exitReason = "config.sw_Home interrupt"
-    if trigger == 512:
-        if not edgeDetect & trigger:
-            exitReason = "config.sw_Upper interrupt"
-    if trigger == 1024:
-        exitReason = "config.sw_Lower interrupt"
-    if trigger == 2048:
-        exitReason = "config.sw_Occup interrupt"
-#     if trigger & 4098:
-#         exitReason = "config.tm_Dn_Runtime 0 reached"
+            trigger = Check_Button_Press(checkRunTime, tm)
+            
+            if (ignoredBin & config.ignore_none) and trigger > 0:
+                exitReason = "all triggers are enabled. Activated trigger interrupt = " + str(trigger)
+            else:
+                if not ignoredBin & trigger:
+                    if trigger == 1:
+                        exitReason = "config.sw_Up interrupt"
+                    if trigger == 2:
+                        exitReason = "config.sw_Up2 iterrupt"            
+                    if trigger == 4:
+                        exitReason = "config.sw_Dn interrupt"
+                    if trigger == 8:
+                        exitReason = "config.sw_Dn2 iterrupt"            
+                    if trigger == 16:
+                        exitReason = "config.sw_Main_Up interrupt"
+                    if trigger == 32:
+                        exitReason = "config.sw_Main_Up2 iterrupt"
+                    if trigger == 64:
+                        exitReason = "config.sw_Main_Dn interrupt"
+                    if trigger == 128:
+                        exitReason = "config.sw_Main_Dn2 interrupt"
+                    if trigger == 256:
+                        exitReason = "config.sw_Home interrupt"
+                    if trigger == 512:
+                        exitReason = "config.sw_Upper interrupt"
+                    if trigger == 1024:
+                        exitReason = "config.sw_Lower interrupt"
+                    if trigger == 2048:
+                        exitReason = "config.sw_Occup interrupt"         
+                    if checkRunTime == False and trigger == 4096:
+                        exitReason = "config.id_dn_runtime interrupt: tm_Dn_Runtime reached 0"
+                        
+            if exitReason == "" and trigger == 0 and (RunSeconds(tm, time.ticks_ms(), 2) > seconds):
+                trigger = -1
+                exitReason = "Time (True)"
+
         
     printF(spacer + "EXIT: Completed - Reason = " + exitReason)
-    printF(spacer + "Complete wait time = ", str(RunSeconds(tm, time.ticks_ms())), " seconds - Returning to caller")
+    printF(spacer + "Complete wait time = ", str(round(config.tm_Dn_Runtime, 2)), " seconds - Returning to caller")
     once = False
     return exitReason #== "Time (True)"
 
@@ -203,48 +255,52 @@ def Space(spc):
         str += " "
     return str
 
-def Top_To_Home(spc = 1):
+def Up_To_Out(spc = 1):
     spacer = Space(spc) + "topToHome -> "
     config.rly_Up.value(ON)
-    printF(spacer + "Top_To_Home() activated")
+    printF(spacer + "Up_To_Out() activated")
     printF(spacer + "config.rly_Up ON")
-    printF(spacer + "Waiting " + str(config.tm_out_to_home + config.tm_Dn_Runtime) + " seconds")
+    
+    global tm
     tm = time.ticks_ms()
-    result = Wait_Time(config.tm_out_to_home + config.tm_Dn_Runtime, spc + 1, 0, config.tm_1_wait) #ignore sw_Home for 1 second
+    if config.sw_Home.value() == OFF:
+        printF(spacer + "Waiting " + str(config.tm_out_to_home - abs(config.tm_Dn_Runtime)) + " seconds")
+        result = Wait_Time(config.tm_out_to_home - abs(config.tm_Dn_Runtime), 1, config.ignore_led_home + config.id_sw_Up + config.id_dn_runtime, config.tm_1_wait, False)
+    else:
+        printF(spacer + "Waiting " + str(config.tm_out_to_home) + " seconds")
+        result = Wait_Time(config.tm_out_to_home + config.tm_Dn_Runtime, spc + 1, config.ignore_led_home, config.tm_1_wait) #ignore sw_Home for 1 second
+        
     config.tm_Dn_Runtime -= RunSeconds(tm, time.ticks_ms())
     printF(spacer, "config.rly_Up OFF")
     config.rly_Up.value(OFF)
     if result != "config.sw_Upper interrupt" and result != "Time (True)":
         printF(spacer, "Out_To_Home interrupt")
         printF(spacer + "EXIT: Returning to caller (l:234)")
-        printF(spacer, str(config.tm_top_wait) + " second wait")
-#         while config.sw_Up.value() == 1 or config.sw_Dn.value() == 1:
-#             time.sleep(.1)
-        return False
+        return result
     else:
         printF(spacer, str(config.tm_top_wait) + " second wait")
-        result = Wait_Time(config.tm_top_wait, spc + 1, 0, config.tm_top_wait + 1)
+        result = Wait_Time(config.tm_top_wait, spc + 1, config.ignore_allLimits, config.tm_top_wait + 1)
 #         printF(spacer, "-- here --")
         if result != "Time (True)":
             printF(spacer, "Out_To_Home TopWait interrupt")
         else:
             printF(spacer, "TopWait complete")
-            Down_To_Home(spc + 1)
-      
-      
+            result = Down_To_Home(spc + 1)
+
     printF(spacer + "EXIT: Returning to caller")
     return result
 
 def Down_To_Home(spc = 1):
     spacer = Space(spc) + "downToHome -> "
     aborted = False
+    global tm
     tm = time.ticks_ms()
     printF(spacer, "goDn_Rcln & GPIO.wait_for_edge(sw_posHome)")
     config.rly_Dn.value(ON)
     printF(spacer, "config.rly_Dn ON")
     
-    ignored = config.id_sw_Dn + config.id_sw_Dn2 + config.id_sw_Main_Dn + config.id_sw_Main_Dn2
-    result = Wait_Time(abs(config.tm_out_to_home), spc + 1, config.id_led_upper, config.tm_1_wait) #Dn, config.tm_1_wait)
+    #ignored = config.ignore_sw_Dn + config.ignore_sw_Dn2 + config.ignore_sw_Main_Dn + config.ignore_sw_Main_Dn2 + config.ignore_led_upper
+    result = Wait_Time(config.tm_out_to_home, spc + 1, config.ignore_sw_Dn, config.tm_1_wait) #Dn, config.tm_1_wait)
     config.tm_Dn_Runtime += RunSeconds(tm, time.ticks_ms())
     config.rly_Dn.value(OFF)
     printF(spacer, "config.rly_Dn OFF")
@@ -253,10 +309,71 @@ def Down_To_Home(spc = 1):
         config.tm_Dn_Runtime = 0
         printF(spacer, ": complete. At home")
     else:
-        printF(spacer, "Dn timeout reached: ", str(config.tm_out_to_home), ' seconds without home interrupt')
-              
+        if result == "Time (True)":
+            printF(spacer, "Dn timeout reached: ", str(config.tm_out_to_home), ' seconds without home interrupt')
+        else:
+            print(result)
+            if result == "configg.tm_Dn_Runtime reached 0":
+                printF(spacer, "config.tm_Dn_Runtime reached 0")
+                config.tm_Dn_Runtime = 0.0
+            else:
+                printF(spacer, result, "interrupt detected")
     printF(spacer + " EXIT: Returning to caller")
+    return result
                            
+def SelfCheck():
+    ledTime = float(0.15)
+    time.sleep(.75)
+    config.led_occup.value(1)
+    time.sleep(ledTime)
+    config.led_lower.value(1)
+    time.sleep(ledTime)
+    config.led_upper.value(1)
+    time.sleep(ledTime)
+    config.led_home.value(1)
+    time.sleep(.4585)
+    config.led_home.value(0)
+    time.sleep(ledTime)
+    config.led_upper.value(0)
+    time.sleep(ledTime)
+    config.led_lower.value(0)
+    time.sleep(ledTime)
+    config.led_occup.value(0)
+    time.sleep(ledTime)
+    ErrorFlash() # not really an error
 
+def ErrorFlash(id = 0):
+    old_Occ = config.led_occup.value()
+    old_Low = config.led_lower.value()
+    old_Hi = config.led_upper.value()
+    old_Home = config.led_home.value()
+    
+    config.led_occup.value(0)
+    config.led_lower.value(0)
+    config.led_upper.value(0)
+    config.led_home.value(0)
+    
+    for x in [1, 2, 3, 4, 5, 6]:
+        config.led_occup.toggle()
+        config.led_lower.toggle()
+        config.led_upper.toggle()
+        config.led_home.toggle()
+        time.sleep(.1)
 
+    if id > 0:
+        for x in [1, 2, 3, 4, 5, 6, 7, 8]:
+            if id & config.ignore_led_occup:
+                config.led_occup.toggle()
+            if id & config.ignore_led_lower:
+                config.led_lower.toggle()
+            if id & config.ignore_led_upper:
+                config.led_upper.toggle()
+            if id & config.ignore_led_home:
+                config.led_home.toggle()
+            time.sleep(.5)
+        
+    config.led_occup.value(old_Occ)
+    config.led_lower.value(old_Low)
+    config.led_upper.value(old_Hi)
+    config.led_home.value(old_Home)
 
