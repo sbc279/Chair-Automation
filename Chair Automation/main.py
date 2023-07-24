4c6b58a59beeab5a08dc216ba9fd96a9a9a50efd
Stephen B Craver <steve@craver.com>
2023-07-24 11:45:37 -0400

    refactor: Stripped down trial

--- Chair Automation/main.py
+++ Chair Automation/main.py
@@ -21,83 +21,68 @@ DN = config.DN
 onceUp = False
 onceDn = False
 tm = float(0.0)
-ledOn = (config.duty_cycle/100)*65_535
+rlyTm = float(0) 
+x = 0
 
-# Set LED brightness ----------------------------------------
-ledOn = 10000 #(config.duty_cycle/100)*65_535
 pwm_homeLed = PWM(config.led_home) # Attach PWM object on the LED pin
 pwm_reclLed = PWM(config.led_recl) # Attach PWM object on the LED pin
 # pwm_upperLed = PWM(config.led_upper) # Attach PWM object on the LED pin
-# pwm_occupLed = PWM(config.led_occup) # Attach PWM object on the LED pin
+pwm_occupLed = PWM(config.led_occup) # Attach PWM object on the LED pin
 pwm_homeLed.freq(config.ledFreq)
 pwm_reclLed.freq(config.ledFreq)
 # pwm_upperLed.freq(config.ledFreq)
-# pwm_occupLed.freq(config.ledFreq)
-# ---------------------------------------------------------------
+pwm_occupLed.freq(config.ledFreq)
 
-#once = False
 print("main.py IPL: (chair) startup  BETA version " + config.version + "\n")
 
 if config.enableWiFi:
     ip = do_connect()
 
-x = 0
-
-def SetZeroTime():
-    if config.use_sw_RiseHome and config.sw_RiseHome.value() and config.use_sw_ReclHome and config.sw_ReclHome.value():
-        config.tm_Dn_Runtime = 0.0
-        return True
+def Led(ledCtl, state = 0):
+    if state == 0:
+        ledCtl.deinit()
     else:
-        return False
-        
+        ledCtl.duty_u16(config.duty_cycle)
+
 # up relay
 def irq_rly_Up(p):
-    x = 0
-    config.led_occup.value(not p.value())
-#     if not p.value():
-#         pwm_occupLed.duty_u16(10000)
-#     else:
-#         pwm_occupLed.deinit()    
+    config.led_upper.value(not p.value())
+    once = False
+    if p.value() and not once:
+        rlyTm = time.ticks_ms
+        once = True
+    else:
+        config.tm_Dn_Runtime -= RunSeconds(rlyTm, time.time.tick_ms)
+        rlyTm = 0
+        once = False
+
 
 # down relay
 def irq_rly_Dn(p):
-    x = 0
+    once = False
     config.led_upper.value(not p.value())
-#     if not p.value():
-#         pwm_upperLed.duty_u16(10000)
-#     else:
-#         pwm_upperLed.deinit() #   config.led_lower.value(not p.value())
+    if p.value() and not once:
+        rlyTm = time.ticks_ms
+        once = True
+    else:
+        config.tm_Dn_Runtime += RunSeconds(rlyTm, time.time.tick_ms)
+        rlyTm = 0
+        once = False
  
 # home limit 
 def irq_sw_RiseHome(p):
-    if not p.value():
-        pwm_homeLed.duty_u16(10000)
-    else:
-        pwm_homeLed.deinit()
-        
-# # upper limit
-# def irq_sw_Upper(p):
-#     x = 0
-#     if not p.value():
-#         pwm_upperLed.duty_u16(10000)
-#     else:
-#         pwm_upperLed.deinit()
-        
+    x = 0
+    Led(pwm_homeLed, p.value())
+
 # recl limit
 def irq_sw_Recl(p):
     x = 0
-    if not p.value():
-        pwm_reclLed.duty_u16(10000)
-    else:
-        pwm_reclLed.deinit()
+    Led(pwm_reclLed, p.value())
     
 # # occup limit    
-# def irq_sw_Occup(p):
-#     x = 0
-#     if not p.value():
-#         pwm_occupLed.duty_u16(10000)
-#     else:
-#         pwm_occupLed.deinit()  
+def irq_sw_Occup(p):
+    Led(pwm_occupLed, p.value())
+
 #J9
 def irq_J9(p):
     # Immediately remove J9's power...
@@ -108,7 +93,7 @@ def irq_J9(p):
 config.sw_RiseHome.irq(lambda p:irq_sw_RiseHome(p)) 	# interrupt for led_Home
 #config.sw_Upper.irq(lambda p:irq_sw_Upper(p))   # interrupt for led_upper
 config.sw_ReclHome.irq(lambda p:irq_sw_Recl(p))   # interrupt for led_lower
-#config.sw_Occup.irq(lambda p:irq_sw_Occup(p))   # interrupt for led_occup
+config.sw_Occup.irq(lambda p:irq_sw_Occup(p))   # interrupt for led_occup
 config.rly_Up.irq(lambda p:irq_rly_Up(p))		# interrupt for rly_Up
 config.rly_Dn.irq(lambda p:irq_rly_Dn(p))	 	# interrupt for rly_Dn
 config.sw_J9.irq(lambda p:irq_J9(p))			# interrupt for sw_J9
@@ -123,20 +108,25 @@ config.actual_time = time.localtime(time.ticks_ms() + UTC_OFFSET)
 config.tm_Dn_Runtime = 0
 
 def Is_Home(runTime, mute = False):
+    printF("IH    runtime               : ", str(runTime))
+    printF("IH    config.tm_Dn_Runtime  : ", str(config.tm_Dn_Runtime))
+    printF("IH    config.use_sw_RiseHome: ", str(config.use_sw_RiseHome))
+    printF("IH    config.sw_RiseHome    : ", str(config.sw_RiseHome.value()==ON_i))
+    
     runTime = round(runTime, 2)
-    if (runTime >= -0.1 and runTime <= 0.1) or SetZeroTime() :
+    if (runTime >= -0.1 and runTime <= 0.1): # or SetZeroTime() :
         runTime = 0.0
-
     config.tm_Dn_Runtime = runTime
-    if  (config.use_sw_RiseHome and config.sw_RiseHome.value() == OFF_i) or (config.use_sw_ReclHome and config.sw_ReclHome.value() == OFF_i):
+    printF("IH adj-config.tm_Dn_Runtime  : ", str(config.tm_Dn_Runtime))
+    
+    if (config.use_sw_RiseHome and config.sw_RiseHome.value() == ON_i) or runTime > 0: # and (config.use_sw_ReclHome and config.sw_ReclHome.value() == ON_i):
         if not mute:
             printF("main -> ", "sw_RiseHome NOT at Home position. config.tm_Dn_Runtime = ", str(runTime))
-            SetZeroTime()
-            return False
+        return False
     else:
         if not mute:
-            printF("main -> ", "At Home position. config.tm_Dn_Runtime = ", str(runTime))
-            SetZeroTime()        
+            printF("main -> ", "At Home position. config.tm_Dn_Runtime = ", str(config.tm_Dn_Runtime))
+            config.tm_Dn_Runtime = 0
         return True
 print("""
 ------------------- Copyright 2023 CRAVER Engineering. -------------------
@@ -164,21 +154,8 @@ print("""
 
 printF("*started*")
 
-# SelfCheck()
-# time.sleep(.5)
-# config.rly_Up.value(OFF_i)
-# config.rly_Dn.value(OFF_i)
-# config.led_home.value(not config.sw_RiseHome.value())
-# config.led_upper.value(not config.sw_Upper.value())
-# config.led_recl.value(not config.sw_ReclHome.value())
-# config.led_occup .value(not config.sw_Occup.value())
-# 
-# 
-# 
 Is_Home(0)
 print("")
-
-#_thread.start_new_thread(Blink, (2,5))
 
 try:
     while True:
@@ -190,48 +167,38 @@ try:
             SwitchDebounce()
             result = ""
             printF("------------------------- UP Procedure Started -------------------------")
-            #if (config.use_sw_RiseHome and not config.sw_RiseHome.value() and config.tm_Dn_Runtime <= 0) or config.tm_Dn_Runtime <= 0:
-            if(config.use_sw_RiseHome and config.sw_RiseHome.value() == ON_i and config.use_sw_ReclHome and config.sw_ReclHome.value() == ON_i) or (config.use_sw_RiseHome and config.sw_RiseHome.value() == OFF_i) or (config.use_Times and config.tm_Dn_Runtime <= 0):
+            printF("    config.tm_Dn_Runtime  : ", str(config.tm_Dn_Runtime))
+            printF("    config.use_sw_RiseHome: ", str(config.use_sw_RiseHome))
+            printF("    config.sw_RiseHome    : ", str(config.sw_RiseHome.value()==ON_i))
+            printF("Is_Home()             : ", str(Is_Home(config.tm_Dn_Runtime, True)))
+            
+            if config.sw_RclnHome.value() == ON_i:
                 # up 'n out
                 tm = time.ticks_ms()
                 config.rly_Up.value(ON_i)
                 result = Up_To_Out() # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
                 resultStr = result.split(',')[0]
                 resultVal = result.split(',')[1]
-                if resultStr.startswith("Time (True)") > 0: # or 
-                    config.tm_Dn_Runtime -= float(resultVal)
-                else:            
-                    printF("main -> ", resultStr)
-                    config.tm_Dn_Runtime = 0.0
+                #if resultStr.startswith("Time (True)") > 0: # or 
+                #    #config.tm_Dn_Runtime -= float(resultVal)
+                #else:            
+                printF("main -> ", resultStr)
+                    #config.tm_Dn_Runtime = 0.0
      
             else:
                 printF("main -> ", "rly_Up ON")
                 tm = time.ticks_ms()
                 config.rly_Up.value(ON_i)
                 ignorer = config.id_sw_riseHome
-                if config.use_sw_ReclHome:
-                    ignorer -= config.id_sw_reclHome
                 result = Wait_Time(config.tm_Dn_Runtime, 1, config.id_all - ignorer, False)
                 resultStr = result.split(',')[0]
                 resultVal = result.split(',')[1]
                 printF("main -> ", "rly_Up OFF_i")
                 config.rly_Up.value(OFF_i)
-                config.tm_Dn_Runtime -= float(resultVal)
+                #config.tm_Dn_Runtime -= float(resultVal)
                 if config.use_sw_ReclHome and resultStr.find("sw_reclHome"):
-                    config.tm_Dn_Runtime = 0
+                    #config.tm_Dn_Runtime = 0
                     printF( "main -> sw_reclHome interrupt")
-                else:
-                    if config.sw_RiseHome.value() == 1:
-                        config.led_occup.on()
-                        printF("main -> ", "Home position overrun: compensating " + str(config.tm_HomeOverRun) + " seconds...")
-                        time.sleep(.1)
-                        config.rly_Dn.value(ON_i)
-                        printF("main -> ", "rly_Dn ON")
-                        Wait_Time(config.tm_HomeOverRun, 1, 0, config.tm_sw_bounce)
-                        config.rly_Dn.value(OFF_i)
-                        printF("main -> ", "rly_Dn OFF")
-                        printF("main -> overrun complete. ", "rly_Dn OFF")
-                        config.led_occup.off()
                 
                 config.rly_Up.value(OFF_i)
                
@@ -244,25 +211,29 @@ try:
             SwitchDebounce()
             result = ""
             printF("------------------------- DOWN Procedure Started -------------------------")
+            printF("    config.tm_Dn_Runtime  : ", str(config.tm_Dn_Runtime))
+            printF("    config.use_sw_RiseHome: ", str(config.use_sw_RiseHome))
+            printF("    config.sw_RiseHome    : ", str(config.sw_RiseHome.value()==ON_i))
             resultStr = ""
             resultVal = float(0.0)
             printF("main -> ", "1:sw_Dn pressed")
-            if (config.use_sw_RiseHome and config.sw_RiseHome.value() == OFF_i) or (config.use_Times and config.tm_Dn_Runtime < 0):
+
+            if config.sw_RiseHome.value() == OFF_i:
                 printF("main -> 2:calling Down_To_Home()")
                 tm = time.ticks_ms()
-                result = Down_To_Home()
+                result = Down_To_Home(config.tm_home_to_out)
                 resultStr = result.split(',')[0]
                 resultVal = result.split(',')[1]
                 config.rly_Dn.value(OFF_i)
-                config.tm_Dn_Runtime += RunSeconds(tm, time.ticks_ms())
+                #config.tm_Dn_Runtime = float(resultVal) #+= RunSeconds(tm, time.ticks_ms())
             else:
-                config.rly_Dn.value(ON_i)
+                rly_Dn.value(ON_i)
                 printF("main -> ", "3:rly_Dn ON")
                 tm = time.ticks_ms()
                 result = Wait_Time(config.tm_down_step, 1, config.id_sw_all) # , config.tm_Dn_Runtime < 0)
                 resultStr = result.split(',')[0]
                 resultVal = result.split(',')[1]
-                config.tm_Dn_Runtime += float(resultVal)
+                #config.tm_Dn_Runtime += float(resultVal)
                 config.rly_Dn.value(OFF_i)
                 printF("main -> ", resultStr)
 
@@ -274,7 +245,7 @@ try:
         while Check_Button_Press() & config.id_sw_Main_Up + config.id_sw_Main_Up2 + config.id_sw_Up2:
             if not onceUp:
                 tm = time.ticks_ms()
-                config.rly_Up.value(ON_i)
+                rly_Up.value(ON_i)
                 printF("-------------------- MAIN UP --------------------")
                 printF("main -> ", "sw_Main_Up pressed")
                 onceUp = True
@@ -282,10 +253,9 @@ try:
         # Up button released...
         if onceUp is True:
             config.rly_Up.value(OFF_i)
-            config.tm_Dn_Runtime -= RunSeconds(tm, time.ticks_ms())
+            #config.tm_Dn_Runtime -= RunSeconds(tm, time.ticks_ms())
             printF("main -> ", "sw_Main_Up released (", str(config.tm_Dn_Runtime), ") seconds")
-            #Is_Home(config.tm_Dn_Runtime)
-            printF("")
+            printF("Home: ", Is_Home(config.tm_Dn_Runtime, True))
             onceUp = False
 
 # Main DN switch...         
@@ -300,10 +270,9 @@ try:
         # Dn button released...
         if onceDn is True:
             config.rly_Dn.value(OFF_i)
-            config.tm_Dn_Runtime += RunSeconds(tm, time.ticks_ms())
+            #config.tm_Dn_Runtime += RunSeconds(tm, time.ticks_ms())
             printF("main -> ", "sw_Main_Dn released (", str(config.tm_Dn_Runtime), ") seconds")
-            #Is_Home(config.tm_Dn_Runtime)
-            printF("")
+            printF("Home: ", Is_Home(config.tm_Dn_Runtime, True))
             onceDn = False
 
 # Failsafe...
@@ -312,7 +281,7 @@ try:
             config.led_occup.off()
             config.rly_Up.value(OFF_i)
             config.rly_Dn.value(OFF_i)
-            printF(spacer + " wait -> FAILSAFE TIMEOUT: ", str(config.tm_failSafeSeconds), "second abort")
+            printF("Main -> FAILSAFE TIMEOUT: ", str(config.tm_failSafeSeconds), "second abort")
         
         time.sleep(.1)
         
@@ -340,7 +309,8 @@ except KeyboardInterrupt:
 #     f = open("Errors.txt", "a")
 #     f.write("ERROR: " + str(Argument) + " \nSTerminating main.py \n")
 #     f.close()
+finally:
 
-#finally:
-
+    config.rly_Up.value(OFF_i)
+    config.rly_Dn.value(OFF_i)
 
